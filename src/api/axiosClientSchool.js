f8ec5521e8ba2c8fd7467e7b165078729ddfc134
Danny <dungithn@gmail.com>
2023-10-31 11:19:36 +0700

    Create token in localstorege

--- src/api/axiosClientSchool.js
+++ src/api/axiosClientSchool.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { STATIC_HOST_SCHOOL } from "../constants";
-import { TOKEN } from "../constants";
+
+
+const token = localStorage.getItem('token') ? localStorage.getItem('token') : localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTJjZTM2NjQ1MGNkNjlhYTliNGY4NjEiLCJpYXQiOjE2OTg3MTY5ODIsImV4cCI6MTY5ODcxNzU4MiwidHlwZSI6ImFjY2VzcyJ9.yQOg8JvxgvTboSRxuM0hleBq7pPAEts1GA6aXcDK0iY');
 
 const axiosClientSchool = axios.create({
     baseURL: `${STATIC_HOST_SCHOOL}`,
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${TOKEN}`
+        'Authorization': `Bearer ${token}`
     }
 });
 
